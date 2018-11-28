deb981d5dafaad318f914e41b23d4ee0a0d2b3c4
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-28 15:55:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -317,7 +317,7 @@ client.on('message', message => {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
         .setDescription('**برفكس البوت (-)**')
-        .addField('play', 'لتشغيل اغنية')
+        .addField('1p', 'لتشغيل اغنية')
         .addField('join', 'دخول رومك الصوتي')
         .addField('disconnect', 'الخروج من رومك الصوتي')
         .addField('skip', 'تخطي الأغنية')
