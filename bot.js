2ace76d372a94f399bfb8f3853995eb9a0b05baa
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-28 15:49:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -353,4 +353,19 @@ client.user.setGame(`-help`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
+client.on('message', msg => {
+
+    if (msg.content == '-join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    client.channels.get("517336116664467507").join(); //by : Toxic Codes
+    });
+
 client.login(process.env.BOT_TOKEN);
